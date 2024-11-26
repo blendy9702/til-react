11396c9758469acd257ca84e2ba43fd6011e1796
blendy9702 <blendy9702@gmail.com>
2024-11-26 11:37:29 +0900

    [css 기초]

---
+++ src/components/pop-style.js
@@ -0,0 +1,3 @@
+//   CSS 의 역할을 하는 객체 리터럴 변수명은 파스칼 케이스로 하도록 하자.
+export const TitleStyle = { color: "", FontSize: "12px" };
+export const BodyStyle = { color: "green", FontSize: "11px" };
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import IndexPage from "./pages/IndexPage";
 import CeoPage from "./pages/CeoPage";
+import Pop from "./components/pop";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <IndexPage></IndexPage>
+    <Pop></Pop>
     {/* <CeoPage></CeoPage> */}
   </StrictMode>,
 );
