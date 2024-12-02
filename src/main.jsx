888b014b6b6acc3c54d3b7ec2bb1080a0116ff04
blendy9702 <blendy9702@gmail.com>
2024-12-02 18:06:26 +0900

    [Todo 연습]

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import EventSample3 from "./event-sample/EventSample3";
+import Todo from "./todos/Todo";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <EventSample3 />
+    <Todo />
   </>,
 );
